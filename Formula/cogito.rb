2ebb9aed61315d91d41bd804daa2b4bf44f7b774
Kevin Deisz <kevin.deisz@gmail.com>
2016-09-23 16:09:05 -0400

    Initial commit

---
+++ Formula/cogito.rb
@@ -0,0 +1,20 @@
+class Cogito < Formula
+  desc 'Cogito allows you to define your AWS IAM policies using a smaller, easier-to-read format.'
+  homepage 'https://github.com/localytics/libcogito'
+  url 'https://github.com/localytics/libcogito/archive/v0.0.1.tar.gz'
+  version '0.0.1'
+  sha256 '1d2c8da664317a9b593dbc7ec0e28b4646b17954cbe8577f9beb23767e8e712b'
+
+  head do
+    url 'https://github.com/localytics/libcogito.git'
+    depends_on 'libtool' => :build
+    depends_on 'automake' => :build
+    depends_on 'autoconf' => :build
+  end
+
+  def install
+    system 'autoreconf', '-i'
+    system './configure', "--prefix=#{prefix}"
+    system 'make', 'install'
+  end
+end
---
+++ README.md
@@ -0,0 +1,29 @@
+# Homebrew Formulae
+
+A [Homebrew tap] for Localytics projects.
+
+[Homebrew tap]: https://github.com/Homebrew/brew/blob/master/docs/brew-tap.md
+
+## Usage
+
+Tap this repository:
+
+```sh
+brew tap localytics/formulae
+```
+
+Install the packages it contains like any other Homebrew package:
+
+```sh
+brew install cogito
+```
+
+### Contributing
+
+Bug reports and pull requests are welcome on GitHub at:
+
+* [libcogito](https://github.com/localytics/humidifier)
+
+### License
+
+The code in this repository is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
