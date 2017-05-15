aa2edde3320bc91c4b36b043be09d12b9c0791b0
Kevin Deisz <kevin.deisz@gmail.com>
2017-05-15 17:58:50 -0400

    Bump cogito to v0.1.0

--- Formula/cogito.rb
+++ Formula/cogito.rb
@@ -1,9 +1,9 @@
 class Cogito < Formula
   desc 'Cogito allows you to define your AWS IAM policies using a smaller, easier-to-read format.'
   homepage 'https://github.com/localytics/libcogito'
-  url 'https://github.com/localytics/libcogito', using: :git, tag: 'v0.0.1'
-  version '0.0.1'
-  sha256 '1d2c8da664317a9b593dbc7ec0e28b4646b17954cbe8577f9beb23767e8e712b'
+  url 'https://github.com/localytics/libcogito', using: :git, tag: 'v0.1.0'
+  version '0.1.0'
+  sha256 'f5388e4867e1f20b6f7aa456ba09ce227ba486f4'
 
   depends_on 'libtool' => :build
   depends_on 'automake' => :build
